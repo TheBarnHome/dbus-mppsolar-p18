7d5e7249b3ffd1607cde1697267492a97912d7ae
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2024-06-19 17:56:00 +0000

    fix error in update loop

--- dbus-mppsolar.py
+++ dbus-mppsolar.py
@@ -641,8 +641,13 @@ class DbusMppSolarService(object):
 
     def _update_PI18(self):
        # raw = runInverterCommands(['GS','MOD','FWS'])
-        raw = runInverterCommands(['GS','MOD'], "PI18")
-        logging.warning(raw)
+        try:
+            raw = runInverterCommands(['GS','MOD'], "PI18")
+            logging.warning(raw)
+        except:
+            self._updateInternal()
+            return True
+        
     # data, mode, warnings = raw
         data, mode = raw
         with self._dbusmulti as m:#, self._dbusvebus as v:
