3cf4564dacda0552ce35bd984443cf89205e4327
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2024-06-22 06:50:19 +0000

    fix

--- dbus-mppsolar.py
+++ dbus-mppsolar.py
@@ -137,7 +137,7 @@ class DbusMppSolarService(object):
         logging.warning(f"Connected to inverter on {tty} ({self._invProtocol}), setting up dbus with /DeviceInstance = {deviceinstance}")
         
         # Create the services
-        self._systemDcPower = VeDbusItemImport(dbusconnection(), 'com.victronenergy.system', '/Dc/System/Power')
+        self._systemDcPower = VeDbusItemImport(dbusconnection(), 'com.victronenergy.system', '/Control/DVCC')
         self._dbusinverter = VeDbusService(f'com.victronenergy.inverter.mppsolar-inverter.{tty}', dbusconnection())
         # self._dbusvebus = VeDbusService(f'com.victronenergy.vebus.mppsolar.{tty}', dbusconnection())
         self._dbusmppt = VeDbusService(f'com.victronenergy.solarcharger.mppsolar-charger.{tty}', dbusconnection())
