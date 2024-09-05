1e204f81da62c405829b03d7e189c9802f8387b8
Tristan <parler-02.annoncer@icloud.com>
2024-09-05 15:28:20 +0200

    try to fix the same issue.

--- dbus-mppsolar.py
+++ dbus-mppsolar.py
@@ -397,8 +397,10 @@ class DbusMppSolarService(object):
                 m['/Pv/V'] = data.get('pv1_input_voltage', m['/Pv/V'])
                 m['/Pv/0/P'] = data.get('pv1_input_power', m['/Pv/0/P'])
                 m['/Yield/Power'] = data.get('pv1_input_power', m['/Yield/Power'])
-                m['/Yield/User'] = generated.get('total_generated_energy', float(m['/Yield/User'], 0) * 1000) / 1000
-                m['/Yield/System'] = generated.get('total_generated_energy', float(m['/Yield/System'], 0) * 1000) / 1000
+                if generated.get('total_generated_energy'):
+                    m['/Yield/User'] = generated.get('total_generated_energy') / 1000
+                if generated.get('total_generated_energy'):
+                    m['/Yield/System'] = generated.get('total_generated_energy') / 1000
                 m['/MppOperationMode'] = 2 if (data.get('pv1_input_power', 0) > 0) else 0
                 m['/Link/ChargeCurrent'] =  rated.get('max_charging_current',  m['/Link/ChargeCurrent']) # <- Maximum charge current. Must be written every 60 seconds. Used by GX device if there is a BMS or user limit.
                 m['/Link/ChargeVoltage'] =  rated.get('battery_bulk_voltage',  m['/Link/ChargeVoltage']) # <- Charge voltage. Must be written every 60 seconds. Used by GX device to communicate BMS charge voltages.
